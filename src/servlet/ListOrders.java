9196bcd473213030eea525f2274500d35f3ed574
KameHoi <younesarifi@hotmail.com>
2020-08-16 18:57:17 +0200

    16-08-2020. Ok. 18h57

--- src/servlet/ListOrders.java
+++ src/servlet/ListOrders.java
@@ -19,6 +19,7 @@ public class ListOrders extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+
         ListOrdersForm listOrdersForm = new ListOrdersForm();
         List<OrdersEntity> ordersEntities = listOrdersForm.searchOrders(request);
 
